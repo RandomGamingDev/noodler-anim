84f8b005bedf34d562cc5e2051437e52fdab0ae8
randomgamingdev <randomgamingdev@randomgamingdev.randomgamingdev>
2024-12-17 12:52:30 -0500

    desktop

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useEffect, useRef } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import p5 from "p5";
 
 export default function Canvas() {
-  const canvas_container = useRef(null);
+  const canvas_container: RefObject<HTMLDivElement | null> = useRef(null);
 
   useEffect(() => {
     const s = (sketch: p5) => {
@@ -13,6 +13,8 @@ export default function Canvas() {
     
       sketch.setup = () => {
         const canvas = sketch.createCanvas(200, 200);
+        const canvas_container_dims = [canvas_container.current!.offsetWidth, canvas_container.current!.offsetHeight];
+        canvas.position(canvas_container_dims[0] / 2 - canvas.width / 2, canvas_container_dims[1] / 2 - canvas.height / 2);
         canvas.parent(canvas_container.current!);
       };
     
@@ -26,7 +28,7 @@ export default function Canvas() {
   }, []);
 
   return (
-    <div className="w-full h-full fixed top-0 bg-black ml-[2.5rem] z-[-1]" ref={canvas_container}>
+    <div className="w-full h-full fixed top-0 bg-gray-950 ml-[2.5rem] mr-[15rem] z-[-1]" ref={canvas_container}>
 
     </div>
   );
